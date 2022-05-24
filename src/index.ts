fcfd8387499c98bd51ba6b503e5d30923ec28396
Alexander Swensen <alex.swensen@gmail.com>
2022-05-24 09:38:16 -0500

    use commonjs

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
 #!/usr/bin/env node
-import { Command } from "commander";
-import { URL } from "url";
-
-const __filename = new URL("", import.meta.url).pathname;
-const __dirname = new URL(".", import.meta.url).pathname;
+import "./env";
 
 console.log(__dirname);
-
-const program = new Command();
-
-program
-  .description("Uploads files to Mailgun")
-  .option("-f, --file <file>", "File to upload")
-  .addHelpCommand();
-
-program.parse();
